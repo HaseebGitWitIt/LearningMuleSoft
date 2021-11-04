0d62fc778fb1cd1dc920e3dcee60e1c360cc6b35
Haseeb <haseeb01001@gmail.com>
2021-11-04 17:43:36 -0400

    Called Jva

---
+++ src/main/java/demo20211104v7/StringOperations.java
@@ -0,0 +1,13 @@
+package demo20211104v7;
+
+public class StringOperations {
+	
+	public static String concat(String a, String b) {
+		return a+" " +b;
+	}
+	
+	public String multiConcat(String a, String b, String c) {
+		return a+" "+b+" "+c;
+	}
+
+}
